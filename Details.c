eb0a3ffc468c766aa7e2c554a5b2ed3210dbf7c0
Lennis Bahati <lennisbahati@gmail.com>
2025-09-19 13:45:59 +0300

    Add files via upload

---
+++ Details.c
@@ -0,0 +1,50 @@
+//My 'Persanol Details
+/*
+Name : Lennis Bahati
+Reg No. PA106/G/28806/25
+Description:My personal details
+*/
+#include<stdio.h> 
+int main()
+{
+	printf("Name: Lennis Bahati \n");
+	printf("Please enter the following details \n");
+	
+int Height;
+    printf("Height:");
+    scanf("%d",&Height);
+    printf(" Height is %d\n" , Height );
+    
+
+int ID ;
+   printf("ID :");
+   scanf("%d",&ID );
+   printf(" ID  is %d\n",ID );
+   
+int Helb;
+    printf("Helb;");
+    scanf("%d",&Helb);
+    printf(" Helb balance is %d \n",Helb);
+    
+    
+int Location;
+    printf("Location:");
+    scanf("%d",&Location);
+    printf("Location %d \n",Location);
+   return 0;
+   
+}   
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
---
+++ Programmes taken.c
@@ -0,0 +1,22 @@
+//Program to display all units
+/*
+Name : Lennis Bahati
+Reg no: PA106/G/28806/25
+Description: Program to display hello worl
+*/
+#include<stdio.h>
+int main ()
+{
+	printf("NAME; Lennis Bahati \n")
+	printf("Programms : \n")}
+	printf("Discrete Mathematics \n");
+	printf("Mathematics for Science \n");
+	printf("Fundamentals of ICT \n");
+	printf("Computer systems and Organizations \n");
+	printf("Communication Skills \n");
+	printf("Introduction to programming and problem solving \n");
+	printf("Fundamentals System Analysis and Design \n" );
+	
+	
+	return 0;
+}
