5801ae041031a3a9a447b0d664dbbbb4059664e0
Lennis Bahati <lennisbahati@gmail.com>
2025-10-10 21:32:52 +0300

    Add files via upload

---
+++ Forloop.c
@@ -0,0 +1,20 @@
+//counting down from 100 to 50
+/*
+Name:Lennis Bahati
+Reg no:PA106/G/28806/25
+Description :counting down from 100 to 50
+Date:Week 
+*/
+#include <stdio.h>
+
+int main() 
+{
+    int i;
+
+    //counting down from 100 to 50
+    for (i = 100; i >= 50; i--) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
